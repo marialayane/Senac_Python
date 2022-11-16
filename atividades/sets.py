e882542909f7599c3f1fbcf466edce22f1b2dfbd
Maria Silva <lane.silva.int@gmail.com>
2022-11-16 17:47:38 -0300

    Questão 1 de sets

--- atividades/sets.py
+++ atividades/sets.py
@@ -1,1 +1,17 @@
+#1
+"""
+print('-' * 48)
+print('-------Criando um set e adicionando itens-------')
+print('-' * 48)
 
+lista_set = set()
+print('Para criar o set, por favor informe alguns dados.')
+nome = str(input('Informe o nome: '))
+lista_set.add(nome)
+idade = int(input('Informe a idade: '))
+lista_set.add(idade)
+sexo = str(input('Informe o sexo: '))
+lista_set.add(sexo)
+
+print('Set: {}'.format(lista_set))
+"""
